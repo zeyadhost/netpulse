4d2f56968aed0023668b06385f88cea0434a6887
qrexpy <117946311+qrexpy@users.noreply.github.com>
2026-01-09 20:52:18 +0200

    Add packet capture module

---
+++ capture.py
@@ -0,0 +1,43 @@
+from scapy.all import sniff, IP, TCP, UDP
+from queue import Queue
+import threading
+
+class PacketCapture:
+    def __init__(self):
+        self.packet_queue = Queue()
+        self.running = False
+        self.capture_thread = None
+        
+    def packet_handler(self, packet):
+        if IP in packet:
+            packet_size = len(packet)
+            protocol = "OTHER"
+            
+            if TCP in packet:
+                protocol = "TCP"
+            elif UDP in packet:
+                protocol = "UDP"
+                
+            self.packet_queue.put({
+                'size': packet_size,
+                'protocol': protocol
+            })
+    
+    def start(self):
+        self.running = True
+        self.capture_thread = threading.Thread(target=self._capture_loop, daemon=True)
+        self.capture_thread.start()
+        
+    def _capture_loop(self):
+        sniff(prn=self.packet_handler, store=False, stop_filter=lambda x: not self.running)
+        
+    def stop(self):
+        self.running = False
+        if self.capture_thread:
+            self.capture_thread.join(timeout=2)
+            
+    def get_packets(self):
+        packets = []
+        while not self.packet_queue.empty():
+            packets.append(self.packet_queue.get())
+        return packets
